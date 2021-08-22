a2d0e07b5fad0c6122c5739e3aa1a57b5a5c6089
Ben Levy <ben.levy@sage.com>
2021-08-22 15:25:33 +0300

    clean up

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const { MongoClient } = require("mongodb");
 
 const listOfProducts = require("./data/products");
+const listOfCountries = require("./data/countries");
 const e = require("express");
 
 const app = express();
@@ -13,59 +14,68 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// const products = require("./routes/products");
-// const cogs = require("./routes/cogs");
-
-// app.use("/products", products);
-// app.use("/cogs", cogs);
-
 //Get Products
 app.get("/products", async (req, res) => {
-  loadProducts();
-  const products = await loadProductsCollection();
+  const products = await loadCollection("products");
+  // await products.remove(); to clean the db
   if (!(await products.count())) {
-    loadProducts(products); // init db
+    loadList(products, listOfProducts); // init db
   }
   res.send(await products.find({}).toArray());
   res.send(products);
 });
 
+//Get Countries
+app.get("/countries", async (req, res) => {
+  const countries = await loadCollection("countries");
+  //await countries.remove();to clean the db
+  if (!(await countries.count())) {
+    loadList(countries, listOfCountries); // init db
+  }
+  res.send(await countries.find({}).toArray());
+  res.send(countries);
+});
+
 app.post("/cogs", async (req, res) => {
-  const products = await loadProductsCollection();
+  const products = await loadCollection("products");
   let { id } = req.body;
   products.findOne({ id }, function (err, product) {
     if (!err) {
-      product.cogs = { ...product.cogs, ...req.body.cogs };
-      product.save(function (err) {
-        if (err) {
-          console.log(err);
-          return res.status(400).send(err.message);
+      cogs = {
+        $set: {
+          productName: product.productName,
+          cogs: { ...product.cogs, ...req.body.cogs }
         }
-      });
+      };
+      console.log(cogs);
+      products.updateOne(
+        { id: `${id}` },
+        { $set: req.body },
+        function (err, res) {
+          if (err) throw err;
+          console.log("product updated");
+        }
+      );
     } else {
       console.log(err);
     }
   });
-  //await posts.insertOne({ text: req.body.text, createdAt: new Date() });
   return res.status(201).send();
 });
 
-async function loadProducts(db) {
+async function loadList(db, list) {
   console.log("===INSERT===");
-  const result = await db.insertMany(listOfProducts);
-  console.log(
-    `products were inserted :)`,
-    Object.values(result.insertedIds).length
-  );
+  const result = await db.insertMany(list);
+
   console.log("===DONE===");
 }
 
-async function loadProductsCollection() {
+async function loadCollection(name) {
   const uri =
     "mongodb+srv://abc123:9LAqARdEjeFTdafm@cluster0.1oqdx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
   const client = new MongoClient(uri);
   await client.connect();
-  return client.db().collection("products");
+  return client.db().collection(name);
 }
 
 const port = process.env.PORT || 5000;
